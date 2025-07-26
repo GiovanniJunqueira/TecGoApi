abf3b766cbaea518f70360bf2c0b7bbe8fb02c08
JeanLima2112 <jeanlima5939@gmail.com>
2025-07-26 18:30:54 -0300

    Feat: Auth Controller

---
+++ src/main/java/com/example/tech_go_api/dto/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package com.example.tech_go_api.dto;
+
+
+public record LoginRequestDTO (String email, String password){}
--- src/main/java/com/example/tech_go_api/repositories/UserRepository.java
+++ src/main/java/com/example/tech_go_api/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.tech_go_api.repositories;
 import com.example.tech_go_api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
