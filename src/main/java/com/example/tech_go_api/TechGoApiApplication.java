95b8e403fc48b131e958379c8d784f2da620439a
JeanLima2112 <jeanlima5939@gmail.com>
2025-07-26 16:16:38 -0300

    Feat: docker and .env config

---
+++ src/main/java/com/example/tech_go_api/TechGoApiApplication.java
@@ -0,0 +1,22 @@
+package com.example.tech_go_api;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechGoApiApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")  // onde está o .env
+				.ignoreIfMissing()
+				.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+		SpringApplication.run(TechGoApiApplication.class, args);
+	}
+
+}
