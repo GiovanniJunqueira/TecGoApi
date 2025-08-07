8342595916d51d65c2f53620220348a8d78cd5b3
JeanLima2112 <jeanlima5939@gmail.com>
2025-08-07 15:58:06 -0300

    Fix: expiration times is .env

--- src/main/java/com/example/tech_go_api/TechGoApiApplication.java
+++ src/main/java/com/example/tech_go_api/TechGoApiApplication.java
@@ -17,6 +17,8 @@ public class TechGoApiApplication {
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		System.setProperty("JWT_ACCESS_TOKEN_DURATION_MINUTES", dotenv.get("JWT_ACCESS_TOKEN_DURATION_MINUTES"));
+		System.setProperty("JWT_REFRESH_TOKEN_DURATION_DAYS", dotenv.get("JWT_REFRESH_TOKEN_DURATION_DAYS"));
 
 		SpringApplication.run(TechGoApiApplication.class, args);
 	}
